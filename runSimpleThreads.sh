ea7a2fc18be87b590a1e803981ebc6df4116889d
Massimo Cappellano <massimo.cappe@gmail.com>
2022-03-19 10:29:31 +0100

    fix

--- runSimpleThreads.sh
+++ runSimpleThreads.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-if [ -z "$1" ];
-then
-	mvn compile exec:java -Dexec.mainClass="trythreads.simple.SimpleThreads"
+
+if [ -z "$1" ]; then
+mvn compile exec:java -Dexec.mainClass="trythreads.simple.SimpleThreads"
 else
-	mvn compile exec:java -Dexec.mainClass="trythreads.simple.SimpleThreads" -Dexec.args="$1"
-if
+mvn compile exec:java -Dexec.mainClass="trythreads.simple.SimpleThreads" -Dexec.args="$1"
+fi
+
+
